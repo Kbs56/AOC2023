153dd6be26590c8baca95a2247456019bc9c8caa
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-12-01 11:34:33 -0600

    Experimenting with file reading

--- Day1/main.go
+++ Day1/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("AOC!")
+	file, err := os.Open("smallInput.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		res := dummyFunc(line)
+		fmt.Println(res)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
+
+func dummyFunc(line string) string {
+	p1 := 0
+
+	return string(line[p1])
 }
