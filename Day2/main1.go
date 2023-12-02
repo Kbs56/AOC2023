53da0d0825ab36aff297e69d1ff045395f7e2989
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-02 11:46:06 -0600

    Day 2 part 1, getting somewhere....... maybe

--- Day2/main1.go
+++ Day2/main1.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Day 2")
+	// file, err := os.Open("testInput.txt")
+	file, err := os.Open("smallInput.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// fmt.Println(line)
+		gameID := getGameID(line)
+		fmt.Println(gameID)
+
+		cubes := getDiceInGame(line)
+
+		for _, str := range cubes {
+			fmt.Println(strings.TrimSpace(str))
+		}
+	}
+}
+
+func getGameID(line string) int {
+	gameID := line[5]
+	gameIDNum, _ := strconv.Atoi(string(gameID))
+	return gameIDNum
+}
+
+func getDiceInGame(line string) []string {
+	dice := []string{}
+	_, after, _ := strings.Cut(line, ": ")
+	new := strings.Split(after, ";")
+	for _, str := range new {
+		dice = append(dice, str)
+	}
+	return dice
 }
