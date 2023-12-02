449fde0c033078c441292ad03bd6a92f37815bb9
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-02 15:52:45 -0600

    Day 2 part 1 done. Yikes.

--- Day2/main1.go
+++ Day2/main1.go
@@ -8,9 +8,13 @@ import (
 	"strings"
 )
 
+type Dice struct {
+	Count int
+	Color string
+}
+
 func main() {
-	// file, err := os.Open("testInput.txt")
-	file, err := os.Open("smallInput.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -19,24 +23,39 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-
-		// fmt.Println(line)
 		gameID := getGameID(line)
-		fmt.Println(gameID)
-
 		rounds := getDiceInGame(line)
-
-		for _, round := range rounds {
-			fmt.Println(strings.TrimSpace(round))
+		isValid := checkValidRounds(rounds)
+		if isValid {
+			sum += gameID
 		}
 	}
+	fmt.Println(sum)
+}
+
+func checkValidRounds(rounds []string) bool {
+	diceMap := map[string]int{"red": 12, "green": 13, "blue": 14}
+	for _, round := range rounds {
+		die := strings.Split(strings.TrimSpace(round), ", ")
+		for _, dice := range die {
+			count, _ := strconv.Atoi(strings.Split(dice, " ")[0])
+			color := strings.Split(dice, " ")[1]
+			dice := &Dice{Count: count, Color: color}
+			if dice.Count > diceMap[dice.Color] {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func getGameID(line string) int {
-	gameID := line[5]
-	gameIDNum, _ := strconv.Atoi(string(gameID))
+	before, _, _ := strings.Cut(line, ": ")
+	gameString := strings.Split(before, " ")[1]
+	gameIDNum, _ := strconv.Atoi(string(gameString))
 	return gameIDNum
 }
 
