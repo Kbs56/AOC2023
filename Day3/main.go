b9fd81523e1e2418b051d61faa8dd049ef7a9efa
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-03 14:47:08 -0600

    setting up still, need to think about the problem now

--- Day3/main.go
+++ Day3/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Day 3")
+	// file, err := os.Open("flatInput.txt")
+	// file, err := os.Open("input.txt")
+	file, err := os.Open("testInput.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	arr := [][]string{}
+	for scanner.Scan() {
+		lines := scanner.Text()
+		line := strings.Split(lines, "")
+		arr = append(arr, line)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if _, err := strconv.Atoi(arr[i][j]); err != nil && arr[i][j] != "." {
+				fmt.Println(arr[i][j])
+				// check adjacent values
+				// up, down, left, right & diags
+			}
+		}
+	}
 }
