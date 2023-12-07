5c4972a592b216ce309d8060f8cd5092dfd8c8ac
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-07 07:13:20 -0600

    alrightalrightalright

--- Day5/main.go
+++ Day5/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -23,13 +24,28 @@ func main() {
 	fmt.Println(seeds)
 
 	// something to hold list of mappings here
+	mappings := [][]int{}
 
 	for i := 2; i < len(lines); i++ {
-		for i < len(lines) && len(lines[i]) > 0 {
-			fmt.Println(lines[i])
+		for i < len(lines) && len(lines[i]) > 0 && unicode.IsNumber(rune(lines[i][0])) {
+			// fmt.Println(lines[i])
+			stringArr := strings.Fields(lines[i])
+			nums := makeIntArray(stringArr)
+			fmt.Println(nums)
+			mappings = append(mappings, nums)
 			i++
 		}
 	}
+	fmt.Println(mappings)
+}
+
+func makeIntArray(arr []string) []int {
+	result := []int{}
+	for _, num := range arr {
+		number, _ := strconv.Atoi(num)
+		result = append(result, number)
+	}
+	return result
 }
 
 func getSeedNumbers(line string) []int {
