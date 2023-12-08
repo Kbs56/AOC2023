ac69cad7bfbaf0b8505db1ebff2557e330c57a71
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-07 20:56:01 -0600

    Day 6 pt 1

--- Day6/main.go
+++ Day6/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
+	contents, _ := os.ReadFile("input.txt")
+
+	lines := strings.Split(string(contents), "\n")
+
+	times := parseLine(lines[0])
+	distances := parseLine(lines[1])
+
+	combos := []int{}
+
+	for i := range times {
+		numCombos := calcWinningCombos(times[i], distances[i])
+		combos = append(combos, numCombos)
+	}
+
+	ans := 1
+	for _, num := range combos {
+		ans *= num
+	}
+	fmt.Println(ans)
+}
+
+func convertToIntArr(arr []string) []int {
+	result := []int{}
+	for _, num := range arr {
+		number, _ := strconv.Atoi(num)
+		result = append(result, number)
+	}
+	return result
+}
+
+func parseLine(line string) []int {
+	_, lineAfter, _ := strings.Cut(line, ":")
+	lineStr := strings.Fields(lineAfter)
+	nums := convertToIntArr(lineStr)
+	return nums
+}
+
+func calcWinningCombos(time, distance int) int {
+	totalWins := 0
+	for i := 1; i < time; i++ {
+		timeRemaining := time - i
+		distanceToTravel := i * timeRemaining
+		if distanceToTravel > distance {
+			totalWins++
+		}
+	}
+	return totalWins
 }
