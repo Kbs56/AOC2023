52575b6789500f7acd6ba36ab641b2014a5cc5f2
Kbs56 <kenny.sheldon99@gmail.com>
2023-12-10 16:33:25 -0600

    Day 7 part 1 pretty slick

--- Day7/main.go
+++ Day7/main.go
@@ -4,18 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 const (
-	HIGH_CARD      int = 0
-	ONE_PAIR           = 1
-	TWO_PAIR           = 2
-	THREE_PAIR         = 3
-	FULL_HOUSE         = 4
-	FOUR_OF_A_KIND     = 5
-	FIVE_OF_A_KIND     = 6
+	HIGH_CARD       int = 0
+	ONE_PAIR            = 1
+	TWO_PAIR            = 2
+	THREE_OF_A_KIND     = 3
+	FULL_HOUSE          = 4
+	FOUR_OF_A_KIND      = 5
+	FIVE_OF_A_KIND      = 6
 )
 
 type Hand struct {
@@ -24,8 +25,10 @@ type Hand struct {
 	strength int
 }
 
+var cards = "AKQJT98765432"
+
 func main() {
-	file, _ := os.Open("testInput.txt")
+	file, _ := os.Open("input.txt")
 
 	scanner := bufio.NewScanner(file)
 
@@ -41,8 +44,14 @@ func main() {
 		handStrength := calcHandStrength(hand)
 		currentHand := Hand{cards: hand, bid: bid, strength: handStrength}
 		allHands = append(allHands, currentHand)
+		sortHands(allHands)
 	}
-	fmt.Println(allHands)
+
+	sum := 0
+	for i := 0; i < len(allHands); i++ {
+		sum += allHands[i].bid * (i + 1)
+	}
+	fmt.Println(sum)
 }
 
 func calcHandStrength(hand []string) int {
@@ -55,15 +64,56 @@ func calcHandStrength(hand []string) int {
 			handMap[ch] = 1
 		}
 	}
-	fmt.Println(handMap)
 	handStrength := parseHandMap(handMap)
 	return handStrength
 }
 
 func parseHandMap(handMap map[string]int) int {
-	return 0
+	switch len(handMap) {
+	case 1:
+		return FIVE_OF_A_KIND
+	case 2:
+		for _, v := range handMap {
+			if v == 3 {
+				return FULL_HOUSE
+			}
+		}
+		return FOUR_OF_A_KIND
+	case 3:
+		for _, v := range handMap {
+			if v == 3 {
+				return THREE_OF_A_KIND
+			}
+		}
+		return TWO_PAIR
+	case 4:
+		return ONE_PAIR
+	case 5:
+		return HIGH_CARD
+	default:
+		return 0
+	}
 }
 
-// Sort the Hands (asc) into []Hand
-// Figure out how to settle tiebreakers
-// multiply each hands bid by index + 1
+func sortHands(allHands []Hand) {
+	sort.Slice(allHands, func(i, j int) bool {
+		if allHands[i].strength != allHands[j].strength {
+			return allHands[i].strength < allHands[j].strength
+		}
+		for idx := range allHands[i].cards {
+			if allHands[i].cards[idx] != allHands[j].cards[idx] {
+				return strings.Index(
+					cards,
+					allHands[i].cards[idx],
+				) > strings.Index(
+					cards,
+					allHands[j].cards[idx],
+				)
+			} else {
+				continue
+			}
+		}
+		// Will never be reached due to input
+		return allHands[i].strength < allHands[j].strength
+	})
+}
