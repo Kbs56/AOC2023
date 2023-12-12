ebb5046e7fd934c12fa5645435d0aed6f36ffdf7
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-12-12 14:11:34 -0600

    Notes / planning

--- Day9/main.go
+++ Day9/main.go
@@ -6,28 +6,33 @@ import (
 
 var testArr = []int{10, 13, 16, 21, 30, 45}
 
+// var testArr = []int{1, 3, 6, 10, 15, 21}
+
 func main() {
-	testRet := [][]int{}
-	test := getDiff(testArr, len(testArr), testRet)
+	// Parse input into arrays for each line
+	// Need to do this for each array in the input
+	predictionArr := [][]int{}
+	patternedArr := getDiff(testArr, len(testArr), predictionArr)
 
 	currentNum := 0
-	for i := len(test) - 1; i > 0; i-- {
-		currentNum += test[i-1][len(test[i-1])-1]
+	for i := len(patternedArr) - 1; i > 0; i-- {
+		currentNum += patternedArr[i-1][len(patternedArr[i-1])-1]
 	}
+	// Add current num to a sum
 	fmt.Println(currentNum)
 }
 
-func getDiff(arr []int, length int, testRet [][]int) [][]int {
-	testRet = append(testRet, arr)
+func getDiff(arr []int, length int, predictionArr [][]int) [][]int {
+	predictionArr = append(predictionArr, arr)
 	if checkAllZero(arr) {
-		return testRet
+		return predictionArr
 	}
 	retVals := []int{}
 	for i := 0; i < length-1; i++ {
 		diff := arr[i+1] - arr[i]
 		retVals = append(retVals, diff)
 	}
-	return getDiff(retVals, len(retVals), testRet)
+	return getDiff(retVals, len(retVals), predictionArr)
 }
 
 func checkAllZero(arr []int) bool {
