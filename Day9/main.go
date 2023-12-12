6b85aaa138563fa84c80f919fa481dcb4f26f7a3
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-12-12 10:49:49 -0600

    Maybe I'll use recursion idk

--- Day9/main.go
+++ Day9/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// var testArr = []int{0, 3, 6, 9, 12, 15}
+// var testArr = []int{1, 3, 6, 10, 15, 21}
+var testArr = []int{10, 13, 16, 21, 30, 45}
 
 func main() {
-	fmt.Println("Day 9")
+	test := getDiff(testArr, len(testArr))
+	fmt.Println(test)
+}
+
+func getDiff(arr []int, length int) []int {
+	if checkAllZero(arr) {
+		return arr
+	}
+	retVals := []int{}
+	for i := 0; i < length-1; i++ {
+		diff := arr[i+1] - arr[i]
+		retVals = append(retVals, diff)
+	}
+	return getDiff(retVals, len(retVals))
+}
+
+func checkAllZero(arr []int) bool {
+	for _, num := range arr {
+		if num != 0 {
+			return false
+		}
+	}
+	return true
 }
