60ede8240f6dcdbe9189cf53d30e2a4b53f7e630
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-12-12 14:52:40 -0600

    Day 9 p2 setup

---
+++ Day9B/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		data := strings.Fields(text)
+		numberArr := []int{}
+		for _, num := range data {
+			convertedNumber, _ := strconv.Atoi(num)
+			numberArr = append(numberArr, convertedNumber)
+		}
+		predictionArr := [][]int{}
+		patternedArr := getDiff(numberArr, len(numberArr), predictionArr)
+
+		currentNum := 0
+		for i := len(patternedArr) - 1; i > 0; i-- {
+			currentNum += patternedArr[i-1][len(patternedArr[i-1])-1]
+		}
+		sum += currentNum
+	}
+	fmt.Println(sum)
+}
+
+func getDiff(arr []int, length int, predictionArr [][]int) [][]int {
+	predictionArr = append(predictionArr, arr)
+	if checkAllZero(arr) {
+		return predictionArr
+	}
+	retVals := []int{}
+	for i := 0; i < length-1; i++ {
+		diff := arr[i+1] - arr[i]
+		retVals = append(retVals, diff)
+	}
+	return getDiff(retVals, len(retVals), predictionArr)
+}
+
+func checkAllZero(arr []int) bool {
+	for _, num := range arr {
+		if num != 0 {
+			return false
+		}
+	}
+	return true
+}
