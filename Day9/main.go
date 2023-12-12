42aa875dbfb1a97f8c9ee6642ba6fa1a600aa573
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-12-12 13:43:55 -0600

    ahaaaa?

--- Day9/main.go
+++ Day9/main.go
@@ -4,25 +4,30 @@ import (
 	"fmt"
 )
 
-// var testArr = []int{0, 3, 6, 9, 12, 15}
-// var testArr = []int{1, 3, 6, 10, 15, 21}
 var testArr = []int{10, 13, 16, 21, 30, 45}
 
 func main() {
-	test := getDiff(testArr, len(testArr))
-	fmt.Println(test)
+	testRet := [][]int{}
+	test := getDiff(testArr, len(testArr), testRet)
+
+	currentNum := 0
+	for i := len(test) - 1; i > 0; i-- {
+		currentNum += test[i-1][len(test[i-1])-1]
+	}
+	fmt.Println(currentNum)
 }
 
-func getDiff(arr []int, length int) []int {
+func getDiff(arr []int, length int, testRet [][]int) [][]int {
+	testRet = append(testRet, arr)
 	if checkAllZero(arr) {
-		return arr
+		return testRet
 	}
 	retVals := []int{}
 	for i := 0; i < length-1; i++ {
 		diff := arr[i+1] - arr[i]
 		retVals = append(retVals, diff)
 	}
-	return getDiff(retVals, len(retVals))
+	return getDiff(retVals, len(retVals), testRet)
 }
 
 func checkAllZero(arr []int) bool {
